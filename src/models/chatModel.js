36a26b91a1d80386e6c18be21788957cbd4dde40
TalTikho <196236000+TalTikho@users.noreply.github.com>
2025-04-09 16:10:31 +0300

    all the chat shi (#7)
    * all the chat shi
    
    * addaed manager functionality

--- src/models/chatModel.js
+++ src/models/chatModel.js
@@ -15,25 +15,30 @@ const chatModel = new mongoose.Schema({
         type: Boolean,
         default: false,
     }, //tells if the chat is a group or a regular chat false for chat, true for group
-    manager:
+    manager:[
     { 
         type: mongoose.Schema.Types.ObjectId, ref: 'User',
         required: [true, "Manager of the chat is required"],
-    }, //the user who created the group
+        default: [] // for array of managers in a group
+    }], //the user who created the group
     messages:[
     {
-        type: mongoose.Schema.Types.ObjectId, ref: 'message',
+        type: mongoose.Schema.Types.ObjectId, ref: 'message',// need to change: from message to Message
         default: [],
     }],
     members: 
     [{
-        type: mongoose.Schema.Types.ObjectId, ref: 'users',
+        type: mongoose.Schema.Types.ObjectId, ref: 'users', //  need to change: from eusers to User
         //default: [this.manager],
     }], //list of the members in the chat
     createdAt: 
     { 
         type: Date, default: Date.now 
     }, //date of creation of the chat
+    photo: 
+    {
+        type: String,
+    }
 });
 
-module.exports = mongoose.model('chats', chatModel, 'chats');
+module.exports = mongoose.model('Chat', chatModel, 'chats'); // changed: 'Chat' model name to match standard from 'chats'
--- src/routes/chatRoute.js
+++ src/routes/chatRoute.js
@@ -1,7 +1,41 @@
+// const express = require('express');
+// const router = express.Router();
+// const chatController = require('../controllers/chatController');
+
+// router.route('/').post(chatController.createChat);
+
+// module.exports = router;
 const express = require('express');
 const router = express.Router();
 const chatController = require('../controllers/chatController');
+const { authUser } = require('../controllers/authController');
 
-router.route('/').post(chatController.createChat);
+router.route('/')
+    .post(authUser, chatController.createChat)
+    .get(authUser, chatController.getUserChats);
+
+router.route('/:chatId')
+    .get(authUser, chatController.getChatById);
+
+router.route('/:chatId/update')
+    .put(authUser, chatController.updateChat);
+
+router.route('/:chatId/add-member/:userId')
+    .post(authUser, chatController.addMember);
+
+router.route('/:chatId/add-manager/:userId')
+    .post(authUser, chatController.addManager);
+
+router.route('/:chatId/remove-manager/:userId')
+    .delete(authUser, chatController.removeManager);
+
+router.route('/:chatId/remove-member/:userId')
+    .delete(authUser, chatController.removeMember);
+
+router.route('/:chatId/leave')
+    .delete(authUser, chatController.leaveChat);
+
+router.route('/:chatId/delete')
+    .delete(authUser, chatController.deleteChat);
 
 module.exports = router;
